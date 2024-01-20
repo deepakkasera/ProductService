be0ce1161903eeb7e47d270be79dd8696d969146
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-01-20 23:31:14 +0530

    Product Service

---
+++ src/main/java/com/productservice/productservice/security/SpringSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.productservice.productservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        //.requestMatchers("/products").hasAuthority("admin")
+                        //.anyRequest().authenticated()
+                        //.requestMatchers("/getProducts").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
